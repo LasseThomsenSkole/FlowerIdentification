c75d03aab4ad7028593dd9e604a670c5aeed072e
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-11-11 10:39:17 +0100

    .env api key

--- src/main/java/org/example/floweridentification/controller/IdentificationController.java
+++ src/main/java/org/example/floweridentification/controller/IdentificationController.java
@@ -1,8 +1,13 @@
 package org.example.floweridentification.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class IdentificationController {
+    @Value("${openai.api.key}")
+    private String apikey;
 
 }
