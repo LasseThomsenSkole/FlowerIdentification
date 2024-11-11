867b8554b0bdaa020980a65977985bdabe3f6876
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-11-11 09:42:34 +0100

    innit

---
+++ src/main/java/org/example/floweridentification/FlowerIdentificationApplication.java
@@ -0,0 +1,13 @@
+package org.example.floweridentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerIdentificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerIdentificationApplication.class, args);
+    }
+
+}
