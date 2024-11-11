2c458b45dde283aa1b9c2693fd3542cb826059e0
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-11-11 10:54:45 +0100

    test for env files

--- src/main/java/org/example/floweridentification/controller/IdentificationController.java
+++ src/main/java/org/example/floweridentification/controller/IdentificationController.java
@@ -1,13 +1,24 @@
 package org.example.floweridentification.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class IdentificationController {
     @Value("${openai.api.key}")
     private String apikey;
 
+    private final WebClient webClient;
+
+    public IdentificationController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://api.openai.com/v1/chat/completions").build();
+    }
+    @GetMapping("/apikey")
+    public String getApikey() {
+        return apikey;
+    }
 }
