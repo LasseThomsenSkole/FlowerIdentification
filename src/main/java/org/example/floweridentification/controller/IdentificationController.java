0b819fda213f297df4a6b4fdc46cf90f2d1b3c2f
LasseThomsenSkole <lath0003@stud.kea.dk>
2024-11-12 11:29:53 +0100

    rewrote it all :))))))))))

--- src/main/java/org/example/floweridentification/controller/IdentificationController.java
+++ src/main/java/org/example/floweridentification/controller/IdentificationController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+//https://www.jsonschema2pojo.org/
 @RestController
 @RequestMapping("/")
 public class IdentificationController {
